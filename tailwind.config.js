eb72dc10ae62f175a78551213b92bef825307c60
Godbrand0 <thompsoneregha005@gmail.com>
2024-05-27 20:16:34 -0700

    working on the menu

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}', // Adjust paths to your project structure
+    ],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        cursor: ['responsive'], // Ensure responsive variants are enabled
+      },
+    },
+    plugins: [],
+  };
+  
